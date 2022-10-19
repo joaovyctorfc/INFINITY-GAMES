c311979af5bd0b7146ae5607c952de813989c627
LRNhz <lrnhz23@gmail.com>
2022-10-19 20:19:58 -0300

    att

--- Infinity Games/projeto infinity/src/projeto/infinity/ProjetoInfinity.java
+++ Infinity Games/projeto infinity/src/projeto/infinity/ProjetoInfinity.java
@@ -1,12 +1,12 @@
 
 package projeto.infinity;
 import view.Registrar;
-
+import view.Login;
 public class ProjetoInfinity {
 
  
     public static void main(String[] args) {
-   Registrar obj = new Registrar();
+    Registrar obj = new Registrar();
         
      obj.setVisible(true);   
         
