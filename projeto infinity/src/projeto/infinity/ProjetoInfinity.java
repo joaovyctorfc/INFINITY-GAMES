6e1f6d8e86c8153317cc70cf67b39e75ac017dbd
João Vyctor Ferreira <joaovyctorfc@hotmail.com>
2022-10-19 18:58:39 -0300

    Start do projeto

---
+++ projeto infinity/src/projeto/infinity/ProjetoInfinity.java
@@ -0,0 +1,15 @@
+
+package projeto.infinity;
+import view.Login;
+
+public class ProjetoInfinity {
+
+ 
+    public static void main(String[] args) {
+   Login obj = new Login();
+        
+     obj.setVisible(true);   
+        
+    }
+    
+}
