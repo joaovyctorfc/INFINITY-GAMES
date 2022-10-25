310a3982d337d14b0f739b44fa053cd13460c3fa
João Vyctor Ferreira <joaovyctorfc@hotmail.com>
2022-10-25 09:01:03 -0300

    plano de fundo princ

--- Infinity Games/projeto infinity/src/view/TelaPrincipal.java
+++ Infinity Games/projeto infinity/src/view/TelaPrincipal.java
@@ -14,16 +14,20 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBounds(new java.awt.Rectangle(0, 0, 1600, 900));
+        setUndecorated(true);
+        setPreferredSize(new java.awt.Dimension(1920, 1080));
+        setResizable(false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1498, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 1036, Short.MAX_VALUE)
         );
 
         pack();
