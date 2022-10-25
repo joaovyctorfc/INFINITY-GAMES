1789db69c1e3cf1ec5017644db9cb9ab0b8bb5f0
LRNhz <lrnhz23@gmail.com>
2022-10-25 15:10:32 -0300

    tela pricipal
    Adicionei a imagem e o botao de sair

--- Infinity Games/projeto infinity/src/projeto/infinity/ProjetoInfinity.java
+++ Infinity Games/projeto infinity/src/projeto/infinity/ProjetoInfinity.java
@@ -3,11 +3,12 @@ package projeto.infinity;
 import view.Registrar;
 import view.Login;
 //import view.Splash;
+import view.TelaPrincipal;
 public class ProjetoInfinity {
 
  
     public static void main(String[] args) {
-     Registrar obj = new Registrar();
+     TelaPrincipal obj = new TelaPrincipal();
         
      obj.setVisible(true);   
         
--- Infinity Games/projeto infinity/src/view/TelaPrincipal.java
+++ Infinity Games/projeto infinity/src/view/TelaPrincipal.java
@@ -7,35 +7,48 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
     public TelaPrincipal() {
         initComponents();
+        setLocationRelativeTo(null); 
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBounds(new java.awt.Rectangle(0, 0, 1600, 900));
-        setUndecorated(true);
-        setPreferredSize(new java.awt.Dimension(1920, 1080));
-        setResizable(false);
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1498, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1036, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBounds(new java.awt.Rectangle(0, 0, 0, 0));
+        setUndecorated(true);
+        setPreferredSize(new java.awt.Dimension(1600, 900));
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Captura de tela 2022-10-19 213715.png"))); // NOI18N
+        jButton1.setBorder(null);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1538, 17, -1, -1));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Tela Principal antes de logar.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+     System.exit(0);         // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
+
 }
