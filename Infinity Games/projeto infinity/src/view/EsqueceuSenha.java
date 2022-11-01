86836e99c00db49a69041653188818046113fabb
João Vyctor Ferreira <joaovyctorfc@hotmail.com>
2022-11-01 09:00:00 -0300

    att novas telas

--- Infinity Games/projeto infinity/src/view/EsqueceuSenha.java
+++ Infinity Games/projeto infinity/src/view/EsqueceuSenha.java
@@ -31,23 +31,24 @@ public class EsqueceuSenha extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         emailVeri = new javax.swing.JTextField();
         btnCon = new javax.swing.JButton();
         segCampo = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         novaSenha = new javax.swing.JPasswordField();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setText("Digite seu Email");
-
+        emailVeri.setBackground(new java.awt.Color(217, 217, 217));
+        emailVeri.setFont(new java.awt.Font("Arial", 1, 11)); // NOI18N
+        emailVeri.setBorder(null);
         emailVeri.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 emailVeriActionPerformed(evt);
             }
         });
+        getContentPane().add(emailVeri, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 184, 280, 20));
 
         btnCon.setText("Confirmar");
         btnCon.addActionListener(new java.awt.event.ActionListener() {
@@ -55,59 +56,25 @@ public class EsqueceuSenha extends javax.swing.JFrame {
                 btnConActionPerformed(evt);
             }
         });
+        getContentPane().add(btnCon, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 400, -1, -1));
 
+        segCampo.setBackground(new java.awt.Color(217, 217, 217));
+        segCampo.setFont(new java.awt.Font("Arial", 1, 11)); // NOI18N
+        segCampo.setBorder(null);
         segCampo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 segCampoActionPerformed(evt);
             }
         });
+        getContentPane().add(segCampo, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 248, 280, 20));
 
-        jLabel2.setText("Digite a palavra de segurança");
+        novaSenha.setBackground(new java.awt.Color(217, 217, 217));
+        novaSenha.setFont(new java.awt.Font("Arial", 1, 11)); // NOI18N
+        novaSenha.setBorder(null);
+        getContentPane().add(novaSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 317, 280, 20));
 
-        jLabel3.setText("Digite a nova senha");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(127, Short.MAX_VALUE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(125, 125, 125))
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(novaSenha, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(35, 35, 35)
-                            .addComponent(btnCon))
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(emailVeri)
-                            .addComponent(segCampo)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 174, Short.MAX_VALUE)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(37, 37, 37)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(emailVeri, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(segCampo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel3)
-                .addGap(5, 5, 5)
-                .addComponent(novaSenha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(btnCon)
-                .addContainerGap(67, Short.MAX_VALUE))
-        );
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/RedefinirSenha Tela.png"))); // NOI18N
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -170,9 +137,7 @@ public class EsqueceuSenha extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCon;
     private javax.swing.JTextField emailVeri;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPasswordField novaSenha;
     private javax.swing.JTextField segCampo;
     // End of variables declaration//GEN-END:variables
--- Infinity Games/projeto infinity/src/view/TelaPrincipal.java
+++ Infinity Games/projeto infinity/src/view/TelaPrincipal.java
@@ -7,99 +7,39 @@ public class TelaPrincipal extends javax.swing.JFrame {
 
     public TelaPrincipal() {
         initComponents();
-        setLocationRelativeTo(null); 
     }
+
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton2 = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBounds(new java.awt.Rectangle(1920, 1080, 1920, 1080));
         setUndecorated(true);
-        setResizable(false);
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-<<<<<<< HEAD
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Captura de tela 2022-10-19 213715.png"))); // NOI18N
-        jButton1.setBorder(null);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1538, 22, 30, 30));
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Tela Princ sem log.png"))); // NOI18N
 
-=======
->>>>>>> 60f92a8ba78c557e8712f5f09e101794395ba698
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/botaologarprincipal.png"))); // NOI18N
-        jButton2.setBorder(null);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-<<<<<<< HEAD
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1420, 80, -1, -1));
-=======
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1697, 111, 170, 70));
-
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Captura de tela 2022-10-19 213715.png"))); // NOI18N
-        jButton1.setBorder(null);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(1850, 20, 30, 30));
->>>>>>> 60f92a8ba78c557e8712f5f09e101794395ba698
-
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/botaoregprincipal.png"))); // NOI18N
-        jButton3.setBorder(null);
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-        });
-<<<<<<< HEAD
-        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(1220, 84, -1, -1));
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Tela Principal antes de logar.png"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, -10, -1, 930));
-=======
-        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(1470, 120, 150, -1));
->>>>>>> 60f92a8ba78c557e8712f5f09e101794395ba698
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel3)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel3)
+                .addGap(0, 2, Short.MAX_VALUE))
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-     System.exit(0);         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-      Login login = new Login(); 
-      login.setVisible(true);
-      this.dispose();        
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-       Registrar registrar = new Registrar(); 
-       registrar.setVisible(true);
-       this.dispose();        
-    }//GEN-LAST:event_jButton3ActionPerformed
-
-
-
+ 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
-
 }
