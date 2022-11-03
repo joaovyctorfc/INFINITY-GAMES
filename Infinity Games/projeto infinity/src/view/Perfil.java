eade2aa789ec03271392097df9b6fc8c3cb62621
VHPzin <vanderjr29@gmail.com>
2022-11-03 13:06:28 -0300

    perfil(bug)

--- Infinity Games/projeto infinity/src/view/Perfil.java
+++ Infinity Games/projeto infinity/src/view/Perfil.java
@@ -30,12 +30,11 @@ public class Perfil extends javax.swing.JFrame {
 
         CampoNome = new javax.swing.JTextField();
         CampoEmail = new javax.swing.JTextField();
-        AreaTexto = new javax.swing.JScrollPane();
-        Campotexto = new javax.swing.JTextArea();
-        CampoPalavra = new javax.swing.JTextField();
         CampoSenha = new javax.swing.JTextField();
+        CampoPalavra = new javax.swing.JTextField();
         BotaoInicio = new javax.swing.JButton();
         BotaoFechar = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         ImagemFundo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -50,28 +49,22 @@ public class Perfil extends javax.swing.JFrame {
                 CampoNomeActionPerformed(evt);
             }
         });
-        getContentPane().add(CampoNome, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 230, 430, 20));
+        getContentPane().add(CampoNome, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, 430, 20));
 
         CampoEmail.setBackground(new java.awt.Color(82, 113, 255));
         CampoEmail.setFont(new java.awt.Font("Arial", 1, 11)); // NOI18N
         CampoEmail.setBorder(null);
         getContentPane().add(CampoEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(51, 350, 430, 20));
 
-        Campotexto.setColumns(20);
-        Campotexto.setRows(5);
-        AreaTexto.setViewportView(Campotexto);
-
-        getContentPane().add(AreaTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 220, -1, -1));
+        CampoSenha.setBackground(new java.awt.Color(82, 113, 255));
+        CampoSenha.setFont(new java.awt.Font("Arial", 1, 11)); // NOI18N
+        CampoSenha.setBorder(null);
+        getContentPane().add(CampoSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(51, 459, 430, 20));
 
         CampoPalavra.setBackground(new java.awt.Color(82, 113, 255));
         CampoPalavra.setFont(new java.awt.Font("Arial", 1, 11)); // NOI18N
         CampoPalavra.setBorder(null);
         getContentPane().add(CampoPalavra, new org.netbeans.lib.awtextra.AbsoluteConstraints(51, 568, 430, 20));
-
-        CampoSenha.setBackground(new java.awt.Color(82, 113, 255));
-        CampoSenha.setFont(new java.awt.Font("Arial", 1, 11)); // NOI18N
-        CampoSenha.setBorder(null);
-        getContentPane().add(CampoSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(51, 459, 430, 20));
 
         BotaoInicio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Botao Inicio tela perfil.png"))); // NOI18N
         BotaoInicio.setBorder(null);
@@ -91,6 +84,14 @@ public class Perfil extends javax.swing.JFrame {
         });
         getContentPane().add(BotaoFechar, new org.netbeans.lib.awtextra.AbsoluteConstraints(1140, 0, 30, 30));
 
+        jButton1.setText("Revelar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 650, -1, -1));
+
         ImagemFundo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Tela Perfil.png"))); // NOI18N
         getContentPane().add(ImagemFundo, new org.netbeans.lib.awtextra.AbsoluteConstraints(6, 0, -1, -1));
 
@@ -109,38 +110,52 @@ public class Perfil extends javax.swing.JFrame {
 
     private void CampoNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CampoNomeActionPerformed
  
-	try
-        {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/infinitygames","root","");
-		Statement stm = con.createStatement();
-		ResultSet res = stm.executeQuery("SELECT * from registro");
-		{
-			CampoNome.setText("nome");
-		}
-                
-	}
-	catch(ClassNotFoundException ex)
-	{
-		JOptionPane.showMessageDialog(null,ex.getMessage(),"Erro",JOptionPane.ERROR_MESSAGE);
-	}
-	catch(SQLException ex)
-	{
-		JOptionPane.showMessageDialog(null,ex.getMessage(),"Erro",JOptionPane.ERROR_MESSAGE);
-	}
+	
     }//GEN-LAST:event_CampoNomeActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       String nome = null,senha = null,seg = null,email = null;
+        
+        
+        
+        try
+	   {
+		   Class.forName("com.mysql.cj.jdbc.Driver");
+		   Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/infinitygames?user=root&password=");
+		   java.sql.Statement stm = con.createStatement();
+		   ResultSet res = stm.executeQuery("Select * from registro");
+		   while(res.next())
+		   {
+			   nome = res.getString("nome");
+			   senha = res.getString("senha");
+			   seg = res.getString("seg");
+			   email = res.getString("email");
+		   }
+                   CampoNome.setText(nome);
+                   CampoPalavra.setText(seg);
+                   CampoSenha.setText(senha);
+                   CampoEmail.setText(email);
+	   }
+	   catch(ClassNotFoundException ex)
+	   {
+		   JOptionPane.showMessageDialog(null, ex.getMessage(),"Erro", JOptionPane.ERROR_MESSAGE);
+	   }
+	   catch(SQLException ex)
+	   {
+		   JOptionPane.showMessageDialog(null, ex.getMessage(),"Erro", JOptionPane.ERROR_MESSAGE);
+	   }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JScrollPane AreaTexto;
     private javax.swing.JButton BotaoFechar;
     private javax.swing.JButton BotaoInicio;
     private javax.swing.JTextField CampoEmail;
     private javax.swing.JTextField CampoNome;
     private javax.swing.JTextField CampoPalavra;
     private javax.swing.JTextField CampoSenha;
-    private javax.swing.JTextArea Campotexto;
     private javax.swing.JLabel ImagemFundo;
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
