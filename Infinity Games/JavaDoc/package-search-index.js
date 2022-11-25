f3a12640529fbdbce469aaabed8805c7822ecf36
LRNhz <lrnhz23@gmail.com>
2022-11-25 19:27:21 -0300

    java doc

---
+++ Infinity Games/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"adicionarimagem"},{"l":"All Packages","u":"allpackages-index.html"},{"l":"DAO"},{"l":"DTO"},{"l":"Jogos"},{"l":"projeto.infinity"},{"l":"UTIL"},{"l":"view"}];updateSearchResults();
